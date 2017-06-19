e027d5e5c091db530def8eb79bcb08d5badf210d
Eric Rothfus <eric@rothfus.com>
2017-06-19 13:35:54 -0500

    added initial code for the target

---
+++ firmware/Target/Target.ino
@@ -0,0 +1,62 @@
+#include "Arduino.h"
+#include "RoBoxRomeo.h"
+#include <Servo.h> 
+
+#define IR	11
+#define LED	12
+#define SERVO	10
+ 
+Servo myservo;
+
+IRReceiver irreceiver1(IR);
+
+void setup() 
+{
+     Serial.begin(9600);
+
+     pinMode(LED,OUTPUT);
+     digitalWrite(LED,HIGH);
+     myservo.attach(SERVO);
+     myservo.write(180);
+     delay(1000);
+     myservo.write(100);
+     delay(1000);
+     myservo.write(180);
+	
+     Serial.println("HI!");
+}
+
+int UP = 0;
+unsigned long LED_Blip;
+
+#define LED_BLIP_MS	100
+
+void loop()
+{
+	uint32_t value;
+
+	value = irreceiver1.checkAndReceive();
+
+	if(value) {
+
+	     LED_Blip = millis() + LED_BLIP_MS;
+
+	     digitalWrite(LED,LOW);
+
+	     irreceiver1.Debounce();
+
+	     Serial.println(value,HEX);
+	}
+	if(value == (uint32_t)0xC12F40BF) {
+	     if(UP) {
+		  myservo.write(100);
+	     } else {
+		  myservo.write(180);
+	     }
+	     UP = !UP;
+	}
+
+	if(millis() > LED_Blip) {
+	     digitalWrite(LED,HIGH);
+	}
+}
