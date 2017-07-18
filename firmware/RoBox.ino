4e3d52bf0d91a8bdc77cdd6327b56ce2ea208693
Eric Rothfus <eric@rothfus.com>
2017-07-18 09:05:30 -0500

    moved the RXL development files

--- firmware/RoBox.ino
+++
@@ -1,7 +0,0 @@
-//
-// ROBOX!
-//
-//   This is the main firmware "driver" for Robox, and is intended
-//   to be compiled/downloaded with the Arduino toolset (hence the
-//   .ino extension.
-//
--- firmware/debug.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef DEBUG_H
-#define DEBUG_H
-
-void debugOutput(const char *,...);
-
-#define DEBUGGING
-
-#endif
